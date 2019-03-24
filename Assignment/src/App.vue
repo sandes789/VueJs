2d9ed132031b38e95fddc78975fa4b0d2aeaaa69
Sandesh Shrestha <sansshrestha@gmail.com>
2019-03-24 18:15:54 +0400

    AssignmentOne

---
+++ Assignment/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+   <assignment-one></assignment-one>
+  </div>
+</template>
+
+<script>
+import assignment1 from './components/assignment1';
+
+export default {
+  components:{
+    'assignment-one':assignment1,
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.assignment{
+  width: 500px;
+  height: 180px;
+  margin: auto;
+  border: 1px solid #999;
+  background: #eee;
+  padding: 10px;
+
+}
+</style>
---
+++ Assignment/src/components/assignment1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="assignment1" class="assignment">
+   <h3>{{title}}</h3>
+   <label>A</label>
+   <input type="checkbox" value="I am label A" v-model="messages">
+   <br/>
+   <label>B</label>
+   <input type="checkbox" value="I am label B" v-model="messages">
+
+
+   <div class="preview">
+     <ul>
+       <li v-for="message in messages">{{message}}</li>
+     </ul>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Assignment 1',
+      messages:[]
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
